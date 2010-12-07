07c823a6ca487c9fbcd42dddceef62b97557480d
Elv22 <flipside_22@live.com>
2010-12-06 20:42:14 -0500

    Add Cataclysm debuffs to the healer filter. @Tukz you can probably remove the WOTLK ones or create an addon that re-adds them, i'll leave them in for now.

--- Tukui/config/watch.lua
+++ Tukui/config/watch.lua
@@ -207,6 +207,66 @@ if TukuiCF["unitframes"].raidunitdebuffwatch == true then
 
 			-- Other debuff
 			67479, -- Impale
+			
+			--CATA DEBUFFS
+			--Baradin Hold
+				88942, -- Meteor Slash
+				
+			--Blackwing Descent
+				--Magmaw
+				91911, -- Constricting Chains
+				94679, -- Parasitic Infection
+				94617, -- Mangle
+				
+				--Omintron Defense System
+				79835, --Poison Soaked Shell	
+				91433, --Lightning Conductor
+				91521, --Incineration Security Measure
+				
+				--Maloriak
+				77699, -- Flash Freeze
+				77760, -- Biting Chill
+				
+				--Atramedes
+				92423, -- Searing Flame
+				92485, -- Roaring Flame
+				92407, -- Sonic Breath
+				
+				--Chimaeron
+				82881, -- Break
+				
+				--Nefarian
+				
+			--The Bastion of Twilight
+				--Valiona & Theralion
+				92878, -- Blackout
+				86840, -- Devouring Flames
+				
+				--Halfus Wyrmbreaker
+				
+				--Twilight Ascendant Council
+				92511, -- Hydro Lance
+				82762, -- Waterlogged
+				92505, -- Frozen
+				92518, -- Flame Torrent
+				83099, -- Lightning Rod
+				92075, -- Gravity Core
+				92488, -- Gravity Crush
+				
+				--Cho'gall
+				
+			--Throne of the Four Winds
+				--Conclave of Wind
+					--Nezir <Lord of the North Wind>
+					93131, --Ice Patch
+					--Anshal <Lord of the West Wind>
+					86206, --Soothing Breeze
+					93122, --Toxic Spores
+					--Rohash <Lord of the East Wind>
+					93058, --Slicing Gale 
+				--Al'Akir
+				93260, -- Ice Storm
+				93295, -- Lightning Rod
 		}
 		
 		ORD:RegisterDebuffs(TukuiDB.debuffids)
