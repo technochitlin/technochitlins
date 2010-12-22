ddec22925e020bc7e5cbb6e2c6c33ed8e33d1821
Elv22 <flipside_22@live.com>
2010-12-22 01:21:29 -0500

    Temporary Fix for missing PowerAltBar on Nefarian/Onyxia encounter

--- Tukui/modules/altpowerbar.lua
+++ Tukui/modules/altpowerbar.lua
@@ -5,3 +5,10 @@ PlayerPowerBarAlt:ClearAllPoints()
 PlayerPowerBarAlt:SetPoint("TOP", 0, -32) 
 PlayerPowerBarAlt.ClearAllPoints = TukuiDB.dummy 
 PlayerPowerBarAlt.SetPoint = TukuiDB.dummy
+
+TargetFramePowerBarAlt:ClearAllPoints();
+TargetFramePowerBarAlt:SetPoint("TOP", UIParent, "TOP", 0, -62);
+TargetFramePowerBarAlt:SetParent("UIParent");
+TargetFramePowerBarAlt.ClearAllPoints = TukuiDB.dummy;
+TargetFramePowerBarAlt.SetPoint = TukuiDB.dummy;
+TargetFramePowerBarAlt.SetParent = TukuiDB.dummy;
