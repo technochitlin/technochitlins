3bd378e77869ed6b145048ee7bb14dcd06b4f1d2
Tukz <tukz@tukui.org>
2011-01-07 21:03:43 -0500

    cooldown script update

--- Tukui/modules/functions.lua
+++ Tukui/modules/functions.lua
@@ -314,6 +314,18 @@ function TukuiDB.StyleButton(b, checked)
 	end
 end
 
+TukuiDB.Round = function(number, decimals)
+	if not decimals then decimals = 0 end
+    return (("%%.%df"):format(decimals)):format(number)
+end
+
+TukuiDB.RGBPercToHex = function(r, g, b)
+	r = r <= 1 and r >= 0 and r or 0
+	g = g <= 1 and g >= 0 and g or 0
+	b = b <= 1 and b >= 0 and b or 0
+	return string.format("|cff%02x%02x%02x", r*255, g*255, b*255)
+end
+
 ------------------------------------------------------------------------
 --	unitframes Functions
 ------------------------------------------------------------------------
