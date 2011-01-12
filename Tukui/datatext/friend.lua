0503a60ba0805d51a393f76352626cb6db608dfb
Tukz <tukz@tukui.org>
2011-01-12 11:51:36 -0500

    fix a lua error in datatext/friend.lua when battlenet doesnt tell us the class.

--- Tukui/datatext/friend.lua
+++ Tukui/datatext/friend.lua
@@ -147,6 +147,7 @@ if TukuiCF["datatext"].friends and TukuiCF["datatext"].friends > 0 then
 								for k,v in pairs(LOCALIZED_CLASS_NAMES_FEMALE) do if class == v then class = k end end
 							end
 							classc, levelc = (CUSTOM_CLASS_COLORS or RAID_CLASS_COLORS)[class], GetQuestDifficultyColor(level)
+							if classc == nil then classc = GetQuestDifficultyColor(level) end
 							if UnitInParty(name) or UnitInRaid(name) then grouped = "|cffaaaaaa*|r" else grouped = "" end
 							GameTooltip:AddDoubleLine(format("%s |cff%02x%02x%02x(%d|r |cff%02x%02x%02x%s|r%s) |cff%02x%02x%02x%s|r",client,levelc.r*255,levelc.g*255,levelc.b*255,level,classc.r*255,classc.g*255,classc.b*255,toonName,grouped, 255, 0, 0, status),givenName.." "..surname,238,238,238,238,238,238)
 							if IsShiftKeyDown() then
