4987e6430d6271db6000ab77e413d49ddc1254c8
Tukz <tukz@tukui.org>
2011-02-09 20:45:00 -0500

    re-add Collapse button, but this time, Tukui reskinned.

--- Tukui/modules/blizzard/watchframe.lua
+++ Tukui/modules/blizzard/watchframe.lua
@@ -71,8 +71,21 @@ local function setup()
 
 	WatchFrameTitle:SetParent(TukuiWatchFrame)
 	WatchFrameCollapseExpandButton:SetParent(TukuiWatchFrame)
+	WatchFrameCollapseExpandButton:SetNormalTexture("")
+	WatchFrameCollapseExpandButton:SetPushedTexture("")
+	WatchFrameCollapseExpandButton:SetHighlightTexture("")
+	WatchFrameCollapseExpandButton:SetTemplate("Default")
+	WatchFrameCollapseExpandButton:FontString("text", C.media.font, 12)
+	WatchFrameCollapseExpandButton.text:SetText("X")
+	WatchFrameCollapseExpandButton.text:Point("CENTER", 1, 0)
+	WatchFrameCollapseExpandButton:HookScript("OnClick", function(self) 
+		if WatchFrame.collapsed then 
+			self.text:SetText("V") 
+		else 
+			self.text:SetText("X")
+		end 
+	end)
 	WatchFrameTitle:Kill()
-	WatchFrameCollapseExpandButton:Kill()
 end
 
 ------------------------------------------------------------------------
